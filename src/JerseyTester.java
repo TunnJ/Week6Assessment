876205bb147954c5ba3e16a58845d315128c93b4
kjkleindorfer <kjkleindorfer@dmacc.edu>
2020-10-08 11:21:39 -0500

    See comments in code

--- src/JerseyTester.java
+++ src/JerseyTester.java
@@ -2,11 +2,16 @@ import java.util.List;
 
 import controller.JerseyHelper;
 import model.Jersey;
+import model.Player;
 
 public class JerseyTester {
 
 	public static void main(String[] args) {
+		//Need a player object to insert into the jersey
+		Player sample = new Player("SampleTeam", "SamplePlayer");
 		Jersey j = new Jersey("Purple", "Home");
+		//set the player for the jersey
+		j.setPlayer(sample);
 		JerseyHelper jh = new JerseyHelper();
 		jh.insertJersey(j);
 		
